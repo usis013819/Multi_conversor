23ac3715a3d7d22dc94db492b3edda376e9bdff6
Santos <Santos@DESKTOP-CNOGMDK>
2020-02-19 01:11:14 -0600

    terminado

--- conversor_y_mas/Form1.Designer.cs
+++ conversor_y_mas/Form1.Designer.cs
@@ -28,12 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnconvertir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbde = new System.Windows.Forms.ComboBox();
+            this.cmba = new System.Windows.Forms.ComboBox();
+            this.lblresul = new System.Windows.Forms.Label();
+            this.cmbop = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnconvertir
+            // 
+            this.btnconvertir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnconvertir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnconvertir.Font = new System.Drawing.Font("Forte", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnconvertir.Location = new System.Drawing.Point(152, 343);
+            this.btnconvertir.Name = "btnconvertir";
+            this.btnconvertir.Size = new System.Drawing.Size(99, 43);
+            this.btnconvertir.TabIndex = 0;
+            this.btnconvertir.Text = "Convertir";
+            this.btnconvertir.UseVisualStyleBackColor = false;
+            this.btnconvertir.Click += new System.EventHandler(this.btnconvertir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "De:";
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Font = new System.Drawing.Font("Forte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcantidad.Location = new System.Drawing.Point(168, 205);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(100, 25);
+            this.txtcantidad.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(132, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "A:";
+            // 
+            // cmbde
+            // 
+            this.cmbde.BackColor = System.Drawing.Color.Aquamarine;
+            this.cmbde.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmbde.Font = new System.Drawing.Font("Forte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbde.FormattingEnabled = true;
+            this.cmbde.Location = new System.Drawing.Point(168, 169);
+            this.cmbde.Name = "cmbde";
+            this.cmbde.Size = new System.Drawing.Size(118, 22);
+            this.cmbde.TabIndex = 5;
+            // 
+            // cmba
+            // 
+            this.cmba.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.cmba.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmba.Font = new System.Drawing.Font("Forte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmba.FormattingEnabled = true;
+            this.cmba.Location = new System.Drawing.Point(168, 241);
+            this.cmba.Name = "cmba";
+            this.cmba.Size = new System.Drawing.Size(118, 22);
+            this.cmba.TabIndex = 6;
+            // 
+            // lblresul
+            // 
+            this.lblresul.AutoSize = true;
+            this.lblresul.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresul.Location = new System.Drawing.Point(93, 294);
+            this.lblresul.Name = "lblresul";
+            this.lblresul.Size = new System.Drawing.Size(24, 16);
+            this.lblresul.TabIndex = 7;
+            this.lblresul.Text = "= :";
+            // 
+            // cmbop
+            // 
+            this.cmbop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.cmbop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmbop.Font = new System.Drawing.Font("Forte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbop.FormattingEnabled = true;
+            this.cmbop.Location = new System.Drawing.Point(168, 102);
+            this.cmbop.Name = "cmbop";
+            this.cmbop.Size = new System.Drawing.Size(118, 22);
+            this.cmbop.TabIndex = 8;
+            this.cmbop.SelectedIndexChanged += new System.EventHandler(this.cmbop_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Tipo de convercion:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Forte", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(105, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Multi-Conversor";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(80, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Cantidad:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(349, 398);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbop);
+            this.Controls.Add(this.lblresul);
+            this.Controls.Add(this.cmba);
+            this.Controls.Add(this.cmbde);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnconvertir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnconvertir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbde;
+        private System.Windows.Forms.ComboBox cmba;
+        private System.Windows.Forms.Label lblresul;
+        private System.Windows.Forms.ComboBox cmbop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- conversor_y_mas/Form1.cs
+++ conversor_y_mas/Form1.cs
@@ -12,9 +12,36 @@ namespace conversor_y_mas
 {
     public partial class Form1 : Form
     {
+        opconversor objconversiones = new opconversor();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnconvertir_Click(object sender, EventArgs e)
+        {
+            
+          lblresul.Text = "Valor: " + objconversiones.convertir(cmbde.SelectedIndex, cmba.SelectedIndex, double.Parse(txtcantidad.Text), cmbop.SelectedIndex) + " " + objconversiones.definicion[cmbop.SelectedIndex][cmba.SelectedIndex];
+         
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbop.Items.AddRange(objconversiones.opcion);
+            cmbop.SelectedIndex = 0;
+        }
+
+        private void cmbop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbde.Items.Clear();
+            cmba.Items.Clear();
+            cmbde.Items.AddRange(objconversiones.definicion[cmbop.SelectedIndex]);
+            cmba.Items.AddRange(objconversiones.definicion[cmbop.SelectedIndex]);
+            cmbde.SelectedIndex = 0;
+            cmba.SelectedIndex = 1;
+            lblresul.Text = "?";
+            txtcantidad.Text = "1";
         }
     }
 }
