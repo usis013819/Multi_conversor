b6405815f41e9ef86e36e11d084785677619b484
usis013819 <saymonlenny@gmail.com>
2020-05-01 06:20:32 -0600

    tambien

---
+++ conversor_y_mas/Clase_Parcial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversor_y_mas
+{
+    class Clase_Parcial
+    {
+
+
+    }
+}
